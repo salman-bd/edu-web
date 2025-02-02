c3a3d35276fb6b44005fd2e8cea6d29999198540
salman-bd <abusalman.device@gmail.com>
2025-02-02 22:53:18 +0600

    modify the file

--- src/app/api/profile/delete/[id]/route.ts
+++ src/app/api/profile/delete/[id]/route.ts
@@ -5,31 +5,28 @@ import ProfileModel from "@/models/ProfileModel";
 export const dynamic = 'force-dynamic';
 
 export async function DELETE(request: Request) {  
-
   const url = new URL(request.url);
   const id = url.searchParams.get('id');
 
-  console.log("ID to be deleted:", id);  
-
+  // console.log("ID to be deleted:", id);  
   if (!id) {  
     return NextResponse.json(  
       { message: "Profile ID is required" },  
       { status: 400 }  
-
     );  
   }  
 
   try {  
     await mongoDbConnect();  
     const deletedProfile = await ProfileModel.findByIdAndDelete(id);  
-
     if (!deletedProfile) {  
       return NextResponse.json(  
         { message: "Profile not found" },  
         { status: 404 }  
       );  
     }  
-    console.log("Deleted profile:", deletedProfile);  
+
+    // console.log("Deleted profile:", deletedProfile);  
 
     // Return success response  
     return NextResponse.json(  
@@ -37,8 +34,12 @@ export async function DELETE(request: Request) {
       { status: 200 }  
     );  
 
-  } catch (error) {  
-    console.error("Error deleting profile:", error);  
+  } catch (error: unknown) {  
+    if (error instanceof Error) {
+      console.error("Error deleting profile:", error.message);
+    } else {
+      console.error("Error deleting profile:", error);
+    }
     return NextResponse.json(  
       { message: "Internal server error" },  
       { status: 500 }  
