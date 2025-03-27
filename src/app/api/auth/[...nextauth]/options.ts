ed11111039af7152f60bcdeb12c3d612ac735d96
salmansylhet <abusalman.device@gmail.com>
2025-03-27 22:22:15 +0600

    add the changes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -22,8 +22,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
